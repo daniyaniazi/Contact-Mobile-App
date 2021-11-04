0d581a0cc6f0b7c3c112a361524ffdb681806f1f
Daniya Niazi <farihadania@hotmail.com>
2021-11-04 17:51:35 +0500

    setup server

---
+++ server/app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+require("./contact");
+
+app.use(express.json());
+
+const Contact = mongoose.model("contact");
+
+const mongoUri = "put your url here";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo yeahhh");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("error", err);
+});
+
+app.get("/", (req, res) => {
+  Contact.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/add", (req, res) => {
+  const contact = new Contact({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    position: req.body.position,
+  });
+  contact
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/delete", (req, res) => {
+  Contact.findByIdAndRemove(req.body.id)
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/update", (req, res) => {
+  Contact.findByIdAndUpdate(req.body.id, {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    position: req.body.position,
+  })
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("server running");
+});
---
+++ server/contact.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  picture: String,
+  salary: String,
+  position: String,
+});
+
+mongoose.model("contact", ContactSchema);
